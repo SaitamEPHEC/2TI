2679af30754ffbfed30759a232871e0ffb62d788
Mathias <mathias.gassmann@student.uclouvain.be>
2018-10-04 17:50:31 +0200

    Updaaate avec Adri

--- projet.c
+++ projet.c
@@ -3,6 +3,16 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/time.h>
+#include <limits.h>
+
+struct Voiture {
+	int numero;
+	long best_s1;
+	long best_s2;
+	long best_s3;
+	long best_tour;
+	long total_tour;
+};
 
 long getMicrotime(){
 	struct timeval currentTime;
@@ -14,57 +24,133 @@ double getRandomSeed(){
 	 srand(getMicrotime());
 }
 
-char *timeFormat(){
-	int randomNumber = rand() %20000+30000;
+long randomNumber(){
+	return rand() %10000+40000;
+}
+
+char *timeFormat(long NumberToFormat){
 	//Récupère les minutes du nombre en arrondissant vers le bas
-	int minutes = randomNumber/60000;
-	randomNumber -= minutes*60000;
+	long minutes = NumberToFormat/60000;
+	NumberToFormat -= minutes*60000;
 	//Récupère les secondes du nombre en arrondissant vers le bas
-	int secondes = randomNumber / 1000;
-	randomNumber -= secondes*1000;
+	long secondes = NumberToFormat/1000;
+	NumberToFormat -= secondes*1000;
 	//Récupère les millisecondes du nombre
-	int millisecondes = randomNumber; 
+	long millisecondes = NumberToFormat; 
 	static char minutesChar[30];
 	char secondesChar[10];
 	char millisecondesChar[10];
-	sprintf(minutesChar,"%d : ",minutes);
-	sprintf(secondesChar,"%d : ",secondes);
-	sprintf(millisecondesChar,"%d",millisecondes);
+	if(minutes < 10){
+		sprintf(minutesChar,"0%ld : ",minutes);
+	}
+	else{
+		sprintf(minutesChar,"%ld : ",minutes);
+	}
+	if(secondes < 10){
+		sprintf(secondesChar,"0%ld : ",secondes);
+	}
+	else{
+		sprintf(secondesChar,"%ld : ",secondes);
+	}
+	if(millisecondes < 100){
+		if(millisecondes < 10){
+			sprintf(millisecondesChar,"00%ld",millisecondes);
+		}
+		else{
+			sprintf(millisecondesChar,"0%ld",millisecondes);
+		}
+	}
+	else{
+		sprintf(millisecondesChar,"%ld",millisecondes);
+	}
 	strcat(minutesChar,secondesChar);
 	strcat(minutesChar,millisecondesChar);
 	return minutesChar; 
 }
 
+
+
 int main(int argc, char *argv[]){
 	getRandomSeed();
-	pid_t voiture; 
-	voiture = fork();
+	pid_t v1; 
+	v1 = fork();
 
 	void child_process(void){
-		//Renvoie 15 fois (15 tours) 3 temps (3 secteurs) : Total 45 temps
+		struct Voiture Voiture;
+		
+		//à généraliser pour n'importe quel processus de voiture
+		Voiture.numero = 44;
+		Voiture.best_s1 = LONG_MAX;
+		Voiture.best_s2 = LONG_MAX;
+		Voiture.best_s3 = LONG_MAX;
+		Voiture.best_tour = LONG_MAX;
+
+		//Simulation P1 ==> 1h30 = 
+		while(Voiture.total_tour <= 5400000){
+			long new_s1 = randomNumber();
+			//update best_s1 si meilleur temps que précédent
+			if(Voiture.best_s1 >= new_s1){
+				Voiture.best_s1 = new_s1;	
+			}
+		
+			//update best_s2 si meilleur temps que précédent
+			long new_s2 = randomNumber();
+			if(Voiture.best_s2 >= new_s2){
+				Voiture.best_s2 = new_s2;	
+			}
+
+			//update best_s3 si meilleur temps que précédent
+			long new_s3 = randomNumber();
+			if(Voiture.best_s3 >= new_s3){
+				Voiture.best_s3 = new_s3;	
+			}
+		
+			long new_tour = new_s1 + new_s2 + new_s3;
+			if(Voiture.best_tour >= new_tour){
+				Voiture.best_tour = new_tour;
+			}
+
+			Voiture.total_tour += new_tour;
+			
+			/*printf("Voiture n°%d :\t %s\t %s\t %s\t %s\n", Voiture.numero,
+			timeFormat(new_s1),timeFormat(new_s2),timeFormat(new_s3),
+			timeFormat(new_s1))*/
+
+			//Affichage des temps
+			printf("Voiture n°%d :\t", Voiture.numero);
+			printf("%s\t",timeFormat(new_s1));
+			printf("%s\t",timeFormat(new_s2));
+			printf("%s\t",timeFormat(new_s3));
+			printf("%s\n", timeFormat(new_tour));
+			//Affichage des Meilleur temps
+			printf("MEILLEURS TEMPS : %s\t",timeFormat(Voiture.best_s1));
+			printf("%s\t",timeFormat(Voiture.best_s2));
+			printf("%s\t",timeFormat(Voiture.best_s3));
+			printf("%s\n", timeFormat(Voiture.best_tour));
+			printf("TEMPS TOTAL : %s\n\n", timeFormat(Voiture.total_tour));
+		}
 	}
 
 	void father_process(int child_pid){
 		//Recevoir les infos des processus fils (voitures) et les stocker
 	}
 
-	switch(voiture){
+	switch(v1){
 	//Si erreur lors du fork
-	case -1;
+	case -1:
 	perror("Erreur de fork");
 	return  EXIT_FAILURE;
 	break;
 	//Si on est dans le fils
-	case 0;
+	case 0:
 	child_process();
 	break;
 	//Si on est dans le père
-	default;
-	father_process(pid);
+	default:
+	father_process(v1);
 	break;
 	}
 
-	printf("Le temps de la voiture est  %s\n", timeFormat());
 	return 0;
 }
 
